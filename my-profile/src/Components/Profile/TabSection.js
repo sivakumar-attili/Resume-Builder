145cd695756a0f7569a27b001e93cfab974303c6
unknown <siva.attili123@gmail.com>
2025-03-06 18:24:42 +0530

    profile one more tab updated with tabs

--- my-profile/src/Components/Profile/TabSection.js
+++ my-profile/src/Components/Profile/TabSection.js
@@ -8,9 +8,9 @@ export default function TabSection() {
                 <div className="nav nav-tabs" id="nav-tab" role="tablist">
                     <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">About</button>
                     <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Experience</button>
-                    {/* <button className="nav-link" id="nav-skill-tab" data-bs-toggle="tab" data-bs-target="#nav-skills" type="button" role="tab" aria-controls="nav-skills" aria-selected="false">Skills</button> */}
+   
                     <button className="nav-link" id="nav-awards-tab" data-bs-toggle="tab" data-bs-target="#nav-awards" type="button" role="tab" aria-controls="nav-awards" aria-selected="false">Achievements</button>
-                    <button className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
+                    {/* <button className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button> */}
                 </div>
             </nav>
             <div className="tab-content p-4" id="nav-tabContent">
@@ -31,7 +31,7 @@ export default function TabSection() {
                 <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab"> <div className='mt-2'>
                    <ExpCarousal/>
                 </div></div>
-                <div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">contact</div>
+                {/* <div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">contact</div> */}
                 {/*<div className="tab-pane fade" id="nav-skills" role="tabpanel" aria-labelledby="nav-skills">skills</div> */}
                 <div className="tab-pane fade" id="nav-awards" role="tabpanel" aria-labelledby="nav-awards">Achievements</div>
             </div>
